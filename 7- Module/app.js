b1467e8d1906ded8adc1489b22f9655b5e180fae
Bùi Đình Sơn <86103959+DinhSonBui@users.noreply.github.com>
2022-08-22 18:25:55 +0700

    Add files via upload

---
+++ 7- Module/app.js
@@ -0,0 +1,7 @@
+import logger from './logger/index.js';
+// import {TYPE_LOG,TYPE_WARN, TYPE_ERROR} from './constants.js';
+import * as constants from './constants.js';
+console.log(constants);
+// console.log(logger);
+// logger('Test',TYPE_ERROR);
+// logger('Test',constants.TYPE_ERROR);
---
+++ 7- Module/logger/logger.js
@@ -0,0 +1,7 @@
+import {TYPE_LOG,TYPE_WARN, TYPE_ERROR} from '../constants.js';
+
+function logger(log, type= TYPE_LOG){
+    console[type](log);
+}
+
+export default logger ;
